9c0a1c6fe7f50354827022c0dd1799fbc36c38a0
Rozovod <rozovod.store@mail.ru>
2021-10-22 14:27:34 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Stanislav Kurilenko");
-        System.out.println("31.05.1993");
-
-    }
-}
