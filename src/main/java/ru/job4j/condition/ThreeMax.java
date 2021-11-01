1c2e5946cd3209d05d36e2b1d8e0198857908664
Rozovod <rozovod.store@mail.ru>
2021-11-01 15:06:30 +0300

    4.5. Отладка программы в IDEA [#159464]

---
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class ThreeMax {
+    public static int max(int first, int second, int third) {
+        int result = first;
+        if ((second > first || second == first) && (second > third || second == third)) {
+            result = second;
+        }
+        if (third > first && third > second) {
+            result = third;
+        }
+        return result;
+    }
+
+}
