6e97a63682cc6732a443c5d040c72bdb4f20b92b
Rozovod <rozovod.store@mail.ru>
2021-11-22 14:11:31 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива surname равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Ivan Ivanov";
+        names[2] = "Anna Astahova";
+        names[3] = "Stanislav Kurilenko";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
